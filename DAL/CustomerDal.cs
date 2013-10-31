63c3ff9098bba1dddad164627febf268f327a982
Juliano Bortolozzo Solanho <juliano.solanho@gmail.com>
2013-10-31 20:59:11 -0200

    First concept tests

---
+++ DAL/CustomerDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    using Replay;
+
+
+    public class CustomerDal
+    {
+        [Replay]
+        public List<string> GetAllCustomerNames(int clientId, string storeId)
+        {
+            return new List<string>() { "A", "B", "C", "D" };
+        }
+    }
+}
---
+++ DebugConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugConsole
+{
+    using ProjectUnderTest;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ClassUnderTest().MethodUnderTest();
+        }
+    }
+}
